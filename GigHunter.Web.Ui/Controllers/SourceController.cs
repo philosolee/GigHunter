22eab7f2ff02dd8941b93e370dccce3380f144cb
lw246 <leewilson246@gmail.com>
2019-09-02 10:55:00 +0100

    Extract api url to a property

--- GigHunter.Web.Ui/Controllers/SourceController.cs
+++ GigHunter.Web.Ui/Controllers/SourceController.cs
@@ -1,14 +1,22 @@
 ﻿using GigHunter.Web.Ui.Core.ApiRequests;
 using Microsoft.AspNetCore.Mvc;
 using GigHunter.DomainModels.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace GigHunter.Web.Ui.Controllers
 {
 	public class SourceController : Controller
 	{
+		private readonly string _apiUri;
+
+		public SourceController(IConfiguration configuration)
+		{
+			_apiUri = configuration.GetSection("Uris").GetSection("GigHunterApi").Value;
+		}
+
 		public IActionResult Source()
 		{
-			var sourceRequest = new SourceRequests();
+			var sourceRequest = new SourceRequests(_apiUri);
 			ViewBag.Sources = sourceRequest.GetSources();
 			return View();
 		}
@@ -16,7 +24,7 @@ namespace GigHunter.Web.Ui.Controllers
 		[HttpPost]
 		public IActionResult Source(Source source)
 		{
-			var sourceRequest = new SourceRequests();
+			var sourceRequest = new SourceRequests(_apiUri);
 			sourceRequest.AddSource(source);
 			ViewBag.Sources = sourceRequest.GetSources();
 			return View();
